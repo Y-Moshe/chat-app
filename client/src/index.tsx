c839ec2e3fc04e7512320dd709abd8e544844099
Y-Moshe <moshe.nehemiah@gmail.com>
2022-03-03 00:26:02 +0200

    MUI default typography and react-router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+// MUI default typography
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+ReactDOM.render((
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Router>
+), document.getElementById( 'root' ));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
