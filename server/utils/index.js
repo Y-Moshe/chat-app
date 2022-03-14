184df92e592e8bd6493692360a67b30ae22ac66c
Y-Moshe <moshe.nehemiah@gmail.com>
2022-03-14 23:29:55 +0200

    server utils

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,7 +1,11 @@
 const appVersion = Math.round( require('../package.json').version.slice( 0, 3 ));
 const getFileType = mimeType => (mimeType.match(/\/\w+/ig))[0].slice(1);
+const CustomError = require( './custom-error' );
+const isValidId = require( './is-valid-id' );
 
 module.exports = {
   appVersion,
-  getFileType
+  getFileType,
+  CustomError,
+  isValidId
 };
