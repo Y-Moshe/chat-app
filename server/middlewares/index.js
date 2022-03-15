d3f7a5fb5744fcb904bd638fc55788fc2fab4676
Y-Moshe <moshe.nehemiah@gmail.com>
2022-03-16 00:35:06 +0200

    UPLOADS_FOLDER -> utils

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,10 +1,9 @@
 const auth   = require('./auth');
-const { UPLOADS_FOLDER, upload } = require('./upload');
+const { upload } = require('./upload');
 const errorHandler = require('./error-handler');
 
 module.exports = {
   auth,
-  UPLOADS_FOLDER,
   upload,
   errorHandler
 };
