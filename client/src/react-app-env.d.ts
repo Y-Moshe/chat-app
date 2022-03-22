0c4ff10a33ecaa0ea0a344c408ad6924fe38534a
Y-Moshe <moshe.nehemiah@gmail.com>
2022-03-23 00:15:33 +0200

    define REACT_APP_API_BASE_URL

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    REACT_APP_API_BASE_URL: string;
+  }
+}
+interface Window {
+  Stripe: any;
+}
+  
