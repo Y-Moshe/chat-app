9dd2c3093b7401e1d90e6c2b9c23d7c5884d1a90
Y-Moshe <moshe.nehemiah@gmail.com>
2022-06-15 00:10:54 +0300

    register postMessage event

--- server/src/socket-handlers/index.ts
+++ server/src/socket-handlers/index.ts
@@ -1,12 +1,16 @@
 import { Server, Socket } from 'socket.io';
 
+import { ChatEvents } from './chat-events';
 import registerSetUserAvailability from './set-user-availability';
+import registerPostMessage from './post-message';
 
 export default function( io: Server ) {
   return ( socket: Socket ) => {
+    // User events
     registerSetUserAvailability( io, socket, 'Online' );
-    socket.on( 'disconnect', reason => registerSetUserAvailability( io, socket, 'Offline' ));
-    
-    console.log('Connection Made!');
+    socket.on( 'disconnect', () => registerSetUserAvailability( io, socket, 'Offline' ));
+
+    // Chat events
+    socket.on( ChatEvents.postMessage, message => registerPostMessage( io, socket, message ));
   }
 }
