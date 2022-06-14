a0044e96e127ef0731d28b3aa91215b72b30e220
Y-Moshe <moshe.nehemiah@gmail.com>
2022-06-14 23:18:46 +0300

    set user status on connection\disconnect

--- server/src/socket-handlers/set-user-availability.ts
+++ server/src/socket-handlers/set-user-availability.ts
@@ -1,12 +1,15 @@
 import { Server, Socket } from 'socket.io';
 
 import { User } from '../models';
+import { ChatEvents } from './chat-events';
 
 export default function setUserAvailability( io: Server, socket: Socket,
     availability: 'Online' | 'Offline' ) {
-  if ( availability === 'Online' ) {
+  const isOnline = availability === 'Online' ? true : false;
+  // @ts-ignore
+  const userId = socket.request.user.data._id;
 
-  } else if ( availability === 'Offline' ) {
-
-  }
+  User.updateOne({ _id: userId }, { isOnline })
+    .then(() => io.emit( ChatEvents.userConnected ))
+    .catch( e => console.log( e ));
 };
